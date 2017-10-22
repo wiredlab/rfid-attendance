35c2b808923a556bba3d0d612cffad1fede21f91
Dan White <dan@whiteaudio.com>
2017-10-21 22:11:19 -0500

    add sketch to get MAC address and list visible networks

---
+++ code/esp-scan/esp-scan.ino
@@ -0,0 +1,60 @@
+#include "ESP8266WiFi.h"
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.println("");
+  Serial.print("MAC: ");
+  Serial.print(mac[0],HEX);
+  Serial.print(":");
+  Serial.print(mac[1],HEX);
+  Serial.print(":");
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.println(mac[5],HEX);
+}
+
+void loop()
+{
+
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.println("");
+  Serial.print("MAC: ");
+  Serial.print(mac[0],HEX);
+  Serial.print(":");
+  Serial.print(mac[1],HEX);
+  Serial.print(":");
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.println(mac[5],HEX);
+
+  
+  Serial.print("Scan start ... ");
+  int n = WiFi.scanNetworks();
+  Serial.print(n);
+  Serial.println(" network(s) found");
+  for (int i = 0; i < n; i++)
+  {
+    Serial.println(WiFi.SSID(i));
+  }
+  Serial.println();
+
+  delay(5000);
+}
