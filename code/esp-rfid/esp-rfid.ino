5c00d1e76be2ccc8236c4c0bd43339388ffe52ed
Dan White <dan@whiteaudio.com>
2017-10-22 18:06:17 -0500

    first working version of ESP to reader, needs more cleanup and docs

--- code/esp-rfid/esp-rfid.ino
+++ code/esp-rfid/esp-rfid.ino
@@ -6,44 +6,68 @@
 
   Single shot read - Ask the reader to tell us what tags it currently sees. And it beeps!
 
-  If using the Simultaneous RFID Tag Reader (SRTR) shield, make sure the serial slide
-  switch is in the 'SW-UART' position.
 */
 
-#include <SoftwareSerial.h> //Used for transmitting to the device
-
-SoftwareSerial softSerial(2, 3); //RX, TX
+#include <SoftwareSerial.h> // communication between ESP and RFID reader
 
 #include "SparkFun_UHF_RFID_Reader.h" //Library for controlling the M6E Nano module
-RFID nano; //Create instance
 
-#define BUZZER1 9
-//#define BUZZER1 0 //For testing quietly
-#define BUZZER2 10
+#include "util.h"
+
+
+// jumper wires from:
+//   GPIO 13 to shield 2
+//   GPIO 15 to shield 3
+// select "SW-UART" on both RFID board
+// move UART switch to "HW" on the ESP board
+
+// Create instances of the serial driver and the RFID driver
+SoftwareSerial softSerial(13, 15); //RX, TX (from ESP's view)
+RFID nano;
+
+
+// connect these pins between the ESP and RFID reader:
+//   GPIO 12 to shield 9
+//   GPIO 14 to shield 10
+#define BUZZER1 12
+#define BUZZER2 14
+
+
 
 void setup()
 {
-  Serial.begin(115200);
+  board_init();  // for the functions in util.h
 
+  Serial.begin(115200);  // Serial monitor must match this rate
+
+  // Setup the pins for the buzzer
   pinMode(BUZZER1, OUTPUT);
   pinMode(BUZZER2, OUTPUT);
-
   digitalWrite(BUZZER2, LOW); //Pull half the buzzer to ground and drive the other half.
 
   while (!Serial);
-  Serial.println();
-  Serial.println("Initializing...");
+  status(GOOD);  // blink that we are here
 
-  if (setupNano(38400) == false) //Configure nano to run at 38400bps
+
+  // now get the RFID reader setup
+  Serial.println();
+  Serial.println("Initializing RFID reader...");
+
+  // nano defaults to 115200 baud rate, keep it the same!
+  
+  if (setupNano(115200) == false)
   {
     Serial.println("Module failed to respond. Please check wiring.");
     while (1); //Freeze!
   }
 
   nano.setRegion(REGION_NORTHAMERICA); //Set to North America
-
   nano.setReadPower(500); //5.00 dBm. Higher values may cause USB port to brown out
   //Max Read TX Power is 27.00 dBm and may cause temperature-limit throttling
+
+  // Uncomment this to see the commands and responses sent/received
+  //   between the ESP and Reader
+  //nano.enableDebugging(Serial);
 }
 
 void loop()
@@ -83,6 +107,9 @@ void loop()
   Serial.println(F("]"));
 }
 
+
+
+
 //Gracefully handles a reader that is already configured and already reading continuously
 //Because Stream does not have a .begin() we have to do this outside the library
 boolean setupNano(long baudRate)
@@ -96,20 +123,17 @@ boolean setupNano(long baudRate)
 
   //About 200ms from power on the module will send its firmware version at 115200. We need to ignore this.
   while(softSerial.available()) softSerial.read();
-  
-  nano.getVersion();
 
-  if (nano.msg[0] == ERROR_WRONG_OPCODE_RESPONSE)
-  {
+  nano.getVersion();
+  
+  if (nano.msg[0] == ERROR_WRONG_OPCODE_RESPONSE) {
     //This happens if the baud rate is correct but the module is doing a ccontinuous read
     nano.stopReading();
 
     Serial.println(F("Module continuously reading. Asking it to stop..."));
 
     delay(1500);
-  }
-  else
-  {
+  } else {
     //The module did not respond so assume it's just been powered on and communicating at 115200bps
     softSerial.begin(115200); //Start software serial at 115200
 
@@ -120,11 +144,12 @@ boolean setupNano(long baudRate)
 
   //Test the connection
   nano.getVersion();
-  if (nano.msg[0] != ALL_GOOD) return (false); //Something is not right
+  Serial.print("connection test response: ");
+  Serial.println(nano.msg[0], HEX);
+  //if (nano.msg[0] != ALL_GOOD) return (false); //Something is not right
 
   //The M6E has these settings no matter what
   nano.setTagProtocol(); //Set protocol to GEN2
-
   nano.setAntennaPort(); //Set TX/RX antenna ports to 1
 
   return (true); //We are ready to rock
